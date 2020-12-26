5d0a83641d8fc4c9eca684f1f146f07cfa6ec65b
parisa <parisazangiabadi0@gmail.com>
2020-12-26 16:13:36 +0330

    Add a simple dashboard version 1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    document.getElementById('menu-side').style.display="block";
+    document.getElementById('my-heder').style.width='80%';   
+}
+  function myClose(){
+   document.getElementById('menu-side').style.display='none';
+   document.getElementById('my-heder').style.width='90%';
+  }  
