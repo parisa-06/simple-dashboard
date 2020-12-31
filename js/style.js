f4b42d0c71f3820a606b4db993e1a677294bef02
parisa <parisazangiabadi0@gmail.com>
2020-12-31 15:24:41 +0330

    add progress bar top heder

--- js/style.js
+++ js/style.js
@@ -1,34 +1,44 @@
 
-function openClick(){
-	document.getElementById('sidebar-id').style='right :0px;'
-	document.getElementById('slidebar-open-id').style='display: none;'
+// When the user  icons sidbar, execute myFunction :D
+function openClick() {
+	document.getElementById('sidebar-id').style = 'right :0px;'
+	document.getElementById('slidebar-open-id').style = 'display: none;'
 }
 
-function closClick(){
-	 document.getElementById('sidebar-id').style='right :-250px;'
-	 document.getElementById('slidebar-open-id').style= 'display:block ;'
+function closClick() {
+	document.getElementById('sidebar-id').style = 'right :-250px;'
+	document.getElementById('slidebar-open-id').style = 'display:block ;'
 }
 
-{document.getElementById('main').addEventListener('click',closClick)}
+{ document.getElementById('main').addEventListener('click', closClick) }
 
 
 
+// When the user scrolls the page, execute myFunction :D
+window.onscroll = function () { myFunction() };
 
+function myFunction() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("myBar").style.width = scrolled + "%";
+}
 
-$(function() {
-	var Accordion = function(el, multiple) {
+//When the user accordion the menu sidebar, myFunction :D
+$(function () {
+	var Accordion = function (el, multiple) {
 		this.el = el || {};
 		this.multiple = multiple || false;
 
 		// Variables privadas
 		var links = this.el.find('.byd-link');
 		// Evento
-		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+		links.on('click', { el: this.el, multiple: this.multiple }, this.dropdown)
 	}
 
-	Accordion.prototype.dropdown = function(e) {
+	Accordion.prototype.dropdown = function (e) {
 		var $el = e.data.el;
-			$this = $(this),
+		$this = $(this),
 			$next = $this.next();
 
 		$next.slideToggle();
@@ -37,7 +47,7 @@ $(function() {
 		if (!e.data.multiple) {
 			$el.find('.byd-submenu').not($next).slideUp().parent().removeClass('open');
 		};
-	}	
+	}
 
 	var accordion = new Accordion($('#accordion-id'), false);
 });
